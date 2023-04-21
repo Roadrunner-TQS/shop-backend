76bba29dfed6b67455c38d685f54e2f00f570ecf
v1centebarros <vmabarros@ua.pt>
2023-04-21 22:35:15 +0100

    Springboot setup

---
+++ src/test/java/pt/ua/deti/tqs/shopbackend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
