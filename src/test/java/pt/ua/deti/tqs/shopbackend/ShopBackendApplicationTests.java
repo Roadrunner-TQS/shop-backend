283dcd2dae0c279590f85c479a08e2faac554beb
v1centebarros <vmabarros@ua.pt>
2023-05-11 21:41:39 +0100

    Update POM to work with GH workflow

--- src/test/java/pt/ua/deti/tqs/shopbackend/ShopBackendApplicationTests.java
+++ src/test/java/pt/ua/deti/tqs/shopbackend/ShopBackendApplicationTests.java
@@ -1,9 +1,7 @@
 package pt.ua.deti.tqs.shopbackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ShopBackendApplicationTests {
 
     @Test
