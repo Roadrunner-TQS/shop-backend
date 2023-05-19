69c84c6a5e26268157a717b81c7246a233386cc4
mariana <marianaandrade@ua.pt>
2023-05-19 19:18:23 +0100

    shop service logs

--- src/main/java/pt/ua/deti/tqs/shopbackend/services/ShopService.java
+++ src/main/java/pt/ua/deti/tqs/shopbackend/services/ShopService.java
@@ -44,16 +44,21 @@ public class ShopService {
     }
 
     public List<Category> getCategories(Integer limit) {
+        log.info("ShopService -- getCategories Request");
         if (limit != null) {
+            log.info("ShopService -- getCategories Request with limit: " + limit);
             return categoryRepository.findAll().subList(0, limit);
         }
+        log.info("ShopService -- getCategories Request without limit");
         return categoryRepository.findAll();
     }
 
     public List<Book> getBooks(String sort, String q, Integer limit, Integer page) {
+        log.info("ShopService -- getBooks Request");
         List<Book> result = new ArrayList<>();
         List<Book> books = bookRepository.findAll();
         if (q != null && !q.equals("")) {
+            log.info("ShopService -- getBooks Request with query: " + q);
             for (Book book : books) {
                 if (book.getTitle().toLowerCase().contains(q.toLowerCase())) {
                     result.add(book);
@@ -64,12 +69,15 @@ public class ShopService {
         }
 
         if (sort != null && sort.equals("newest")) {
+            log.info("ShopService -- getBooks Request with sort: " + sort);
             result.sort((Book b1, Book b2) -> b2.getYear().compareTo(b1.getYear()));
         } else if (sort != null && sort.equals("oldest")) {
+            log.info("ShopService -- getBooks Request with sort: " + sort);
             result.sort((Book b1, Book b2) -> b1.getYear().compareTo(b2.getYear()));
         }
 
         if (page != null && limit == null) {
+            log.error("ShopService -- getBooks Request Not Valid");
             return null;
         }
 
@@ -79,39 +87,48 @@ public class ShopService {
             if (end > result.size()) {
                 end = result.size();
             }
+            log.info("ShopService -- getBooks Request with page: " + page + " and limit: " + limit);
             return result.subList(start, end);
         }
 
         if (limit != null) {
+            log.info("ShopService -- getBooks Request with limit: " + limit);
             return result.subList(0, limit);
         }
+        log.info("ShopService -- getBooks Request without limit");
         return result;
     }
 
     public Book getBookById(UUID id) {
+        log.info("ShopService -- getBookById Request");
         return bookRepository.findById(id).orElse(null);
     }
 
     public List<Book> getBooksByCategory(String categorySlug, Integer limit) {
+        log.info("ShopService -- getBooksByCategory Request");
         Category category= categoryRepository.findBySlug(categorySlug).orElse(null);
         if (category == null) {
+            log.error("ShopService -- getBooksByCategory No Category Found");
             return null;
         }
         if (limit != null) {
+            log.info("ShopService -- getBooksByCategory Request with limit: " + limit);
             return bookRepository.findAllByCategories(Optional.of(category)).subList(0, limit);
         }
+        log.info("ShopService -- getBooksByCategory Request without limit");
         return bookRepository.findAllByCategories(Optional.of(category));
     }
     public boolean newOrder(Order orderRequest, String token) {
+        log.info("ShopService -- newOrder Request");
         orderRequest.setDate(System.currentTimeMillis());
         String email = jwtTokenService.getEmailFromToken(token);
         if (email == null) {
-            log.error("User not found");
+            log.error("ShopService -- newOrder No Email Found");
             return false;
         }
         Client client = clientRepository.findByEmail(email).orElse(null);
         if (client == null) {
-            log.error("Client not found");
+            log.error("ShopService -- newOrder No Client Found");
             return false;
         }
         orderRequest.setClient(client);
@@ -124,7 +141,7 @@ public class ShopService {
                 if (optionalBook.isPresent()) {
                     item.setBook(optionalBook.get());
                 } else {
-                    log.error("Book not found");
+                    log.error("ShopService -- newOrder No Book Found");
                     return false;
                 }
             }
@@ -136,47 +153,42 @@ public class ShopService {
         orderR.setTimestamp(System.currentTimeMillis());
         orderStatuses.add(orderR);
         orderStatusRepository.saveAll(orderStatuses);
-        log.info("Order status saved");
 
         paymentRepository.save(orderRequest.getPayment());
-        log.info("Payment saved");
         orderRequest.setPayment(orderRequest.getPayment());
 
         PickUpLocation pickUpLocation =pickUpLocationRepository.findBySlug(orderRequest.getPickUpLocation().getSlug()).orElse(null);
         if (pickUpLocation != null) {
-            log.info("PickUpLocation saved");
             orderRequest.setPickUpLocation(pickUpLocation);
         }
         else{
             pickUpLocationRepository.save(orderRequest.getPickUpLocation());
-            log.info("PickUpLocation saved");
             orderRequest.setPickUpLocation(orderRequest.getPickUpLocation());
         }
         PickUpService pickUpService = pickUpServiceRepository.findById(orderRequest.getPickUpLocation().getPickUpService().getId()).orElse(null);
         if (pickUpService != null) {
-            log.info("PickUpService saved");
             orderRequest.setPickUpService(pickUpService);
         }
         else{
             pickUpServiceRepository.save(orderRequest.getPickUpService());
-            log.info("PickUpService saved");
             orderRequest.setPickUpService(orderRequest.getPickUpService());
         }
         orderRepository.save(orderRequest);
         orderRequest.setOrderStatuses(orderStatuses);
         orderRepository.save(orderRequest);
-        log.info("Order saved");
+        log.info("ShopService -- newOrder Request Success");
         return true;
     }
     public List<OrderDTO> getOrdersByClient(String token) {
+        log.info("ShopService -- getOrdersByClient Request");
         String email = jwtTokenService.getEmailFromToken(token);
         if (email == null) {
-            log.info("User not found");
+            log.info("ShopService -- getOrdersByClient No Email Found");
             return null;
         }
         Client client = clientRepository.findByEmail(email).orElse(null);
         if (client == null) {
-            log.info("Client not found");
+            log.info("ShopService -- getOrdersByClient No Client Found");
             return null;
         }
         List<Order> orders = orderRepository.findAllByClient(client);
@@ -186,6 +198,7 @@ public class ShopService {
             OrderDTO orderDTO = modelMapper.map(order, OrderDTO.class);
             orderDTOS.add(orderDTO);
         }
+        log.info("ShopService -- getOrdersByClient Request Success");
         return orderDTOS;
     }
 }
