5719fd4d15bcc3b94a52fe77496dc5c2b950b58d
mariana <marianaandrade@ua.pt>
2023-05-31 01:54:38 +0100

    auth services implemented and tested

---
+++ src/main/java/pt/ua/deti/tqs/shopbackend/services/JwtTokenService.java
@@ -0,0 +1,70 @@
+package pt.ua.deti.tqs.shopbackend.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.ua.deti.tqs.shopbackend.config.JwtConfig;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class JwtTokenService {
+
+    private final JwtConfig jwtConfig;
+    private final Key jwtKey;
+
+    @Autowired
+    public JwtTokenService(JwtConfig jwtConfig, Key jwtKey) {
+        this.jwtConfig = jwtConfig;
+        this.jwtKey = jwtKey;
+    }
+
+    public String generateToken(String username) {
+        log.info("JwtTokenService -- generateToken -- Request");
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtConfig.getExpiration() * 1000L);
+
+        log.info("JwtTokenService -- generateToken -- Request Success");
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(jwtKey, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean validateToken(String tokenRequest) {
+        log.info("JwtTokenService -- validateToken -- Request received");
+        String token = tokenRequest.replace("Bearer ", "");
+        try {
+            log.info("JwtTokenService -- validateToken --  Request Success");
+            Date expiration = Jwts.parserBuilder().setSigningKey(jwtKey).build().parseClaimsJws(token).getBody().getExpiration();
+            return !expiration.before(new Date());
+        } catch (Exception e) {
+            log.info("JwtTokenService -- validateToken  -- Request Failed");
+            return false;
+        }
+    }
+
+
+    public String getEmailFromToken(String token) {
+        log.info("JwtTokenService -- getEmailFromToken -- Request received");
+        token = token.replace("Bearer ", "");
+        try {
+            log.info("JwtTokenService -- getEmailFromToken -- Request Success");
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(jwtKey).build().parseClaimsJws(token);
+            return claims.getBody().getSubject();
+        } catch (Exception e) {
+            log.info("JwtTokenService -- getEmailFromToken  -- Request Failed");
+            return null;
+        }
+    }
+
+}
+
---
+++ src/test/java/pt/ua/deti/tqs/shopbackend/services/AuthServiceTest.java
@@ -0,0 +1,246 @@
+package pt.ua.deti.tqs.shopbackend.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pt.ua.deti.tqs.shopbackend.data.ClientRepository;
+import pt.ua.deti.tqs.shopbackend.model.Client;
+import pt.ua.deti.tqs.shopbackend.model.auth.LoginRequest;
+import pt.ua.deti.tqs.shopbackend.model.auth.LoginResponse;
+import pt.ua.deti.tqs.shopbackend.model.auth.RegisterRequest;
+import pt.ua.deti.tqs.shopbackend.model.dto.ClientDTO;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+	@Mock(lenient = true)
+	private ClientRepository clientRepository;
+
+	@Mock
+	private JwtTokenService jwtTokenService;
+
+	@InjectMocks
+	private AuthService authService;
+
+	@BeforeEach
+	void setUp() {
+		authService.clearWhiteList();
+	}
+
+	@Test
+	void login_WithValidCredentials_ShouldReturnLoginResponse() {
+		String email = "test@mail.com";
+		String password = "password";
+
+		LoginRequest loginRequest = new LoginRequest(email, password);
+		Client client = new Client();
+		client.setEmail(email);
+		client.setPassword(password);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+		when(jwtTokenService.generateToken(email)).thenReturn("token");
+		when(jwtTokenService.getEmailFromToken("token")).thenReturn(email);
+		when(jwtTokenService.validateToken("token")).thenReturn(true);
+
+		LoginResponse loginResponse = authService.login(loginRequest);
+
+		assertNotNull(loginResponse);
+		assertEquals("token", loginResponse.getToken());
+		assertTrue(authService.isAuthenticated("token"));
+	}
+
+	@Test
+	void login_WithInvalidCredentials_ShouldReturnNull() {
+		String email = "test@mail.com";
+		String password = "password";
+
+		LoginRequest loginRequest = new LoginRequest(email, password);
+
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+		LoginResponse loginResponse = authService.login(loginRequest);
+
+		assertNull(loginResponse);
+		assertFalse(authService.isAuthenticated("token"));
+	}
+	
+	@Test
+	void signup_WithNewUser_ShouldCreateUserAndReturnSuccessMessage() {
+		RegisterRequest registerRequest = new RegisterRequest("test@mail.com", "password", "firstName", "lastName",
+				"phone", null);
+		when(clientRepository.findByEmail(registerRequest.getEmail())).thenReturn(Optional.empty());
+
+		String response = authService.signup(registerRequest);
+
+		assertEquals("User Created", response);
+		verify(clientRepository, times(1)).save(any(Client.class));
+	}
+	
+	@Test
+	void signup_WithExistingUser_ShouldReturnErrorMessage() {
+		RegisterRequest registerRequest = new RegisterRequest("test@mail.com", "password", "firstName", "lastName",
+				"phone", "ROLE_USER");
+		when(clientRepository.findByEmail(registerRequest.getEmail())).thenReturn(Optional.of(new Client()));
+
+		String response = authService.signup(registerRequest);
+
+		assertNull(response);
+		verify(clientRepository, times(0)).save(any(Client.class));
+	}
+
+	@Test
+	void isAuthenticated_WithValidToken_ShouldReturnTrue() {
+		String email = "test@mail.com";
+		String password = "password";
+		RegisterRequest registerRequest = new RegisterRequest(email, password, "firstName", "lastName",
+				"phone", "ROLE_USER");
+
+		authService.signup(registerRequest);
+		Client client = new Client();
+		client.setEmail(email);
+		client.setPassword(password);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+		when(jwtTokenService.generateToken(email)).thenReturn("token");
+
+		String token = authService.login(new LoginRequest(email, password)).getToken();
+
+		when(jwtTokenService.getEmailFromToken(token)).thenReturn(email);
+		when(jwtTokenService.validateToken(token)).thenReturn(true);
+
+		assertTrue(authService.isAuthenticated(token));
+		verify(clientRepository, times(1)).save(any(Client.class));
+	}
+
+	@Test
+	void isAuthenticated_WithInvalidToken_ShouldReturnFalse() {
+		String email = "test@mail.com";
+		String password = "password";
+		Client client = new Client();
+		client.setEmail(email);
+		client.setPassword(password);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+		when(jwtTokenService.generateToken(email)).thenReturn("token");
+
+		String token = authService.login(new LoginRequest(email, password)).getToken();
+
+		String invalid_token = "invalid";
+		when(jwtTokenService.getEmailFromToken(invalid_token)).thenReturn(null);
+
+		assertFalse(authService.isAuthenticated(invalid_token));
+	}
+
+	@Test
+	void logout_WithValidToken_ShouldReturnTrue() {
+		String email = "test@gmail.com";
+		String password = "password";
+		RegisterRequest registerRequest = new RegisterRequest(email, password, "firstName", "lastName",
+				"phone", "ROLE_USER");
+
+		authService.signup(registerRequest);
+		Client client = new Client();
+		client.setEmail(email);
+		client.setPassword(password);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+		when(jwtTokenService.generateToken(email)).thenReturn("token");
+
+		String token = authService.login(new LoginRequest(email, password)).getToken();
+
+		when(jwtTokenService.getEmailFromToken(token)).thenReturn(email);
+
+		assertTrue(authService.logout(token));
+		verify(clientRepository, times(1)).save(any(Client.class));
+	}
+
+	@Test
+	void logout_WithInvalidToken_ShouldReturnFalse() {
+		String email = "test@mail.com";
+		String password = "password";
+		RegisterRequest registerRequest = new RegisterRequest(email, password, "firstName", "lastName",
+				"phone", "ROLE_USER");
+
+		authService.signup(registerRequest);
+		Client client = new Client();
+		client.setEmail(email);
+		client.setPassword(password);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+
+		String invalid_token = "invalid";
+		when(jwtTokenService.getEmailFromToken(invalid_token)).thenReturn(null);
+
+		assertFalse(authService.logout(invalid_token));
+		verify(clientRepository, times(1)).save(any(Client.class));
+	}
+
+	@Test
+    void logout_WithInvalidEmail_ShouldReturnFalse() {
+        Client client = new Client();
+        client.setEmail("test@mail.com");
+        client.setPassword("password");
+
+        LoginRequest loginRequest = new LoginRequest("test@mail.com", "password");
+		
+        when(jwtTokenService.getEmailFromToken("invalid_token")).thenReturn("invalid_email");
+
+        authService.login(loginRequest);
+
+        assertFalse(authService.logout("invalid_token"));
+    }
+
+    @Test
+    void logout_WithOldToken_ShouldReturnFalse() {
+        Client user = new Client();
+        user.setEmail("test@mail.com");
+        user.setPassword("password");
+
+        LoginRequest loginRequest = new LoginRequest("test@mail.com", "password");
+
+        when(jwtTokenService.getEmailFromToken("old_token")).thenReturn(loginRequest.getEmail());
+
+        authService.login(loginRequest);
+
+        assertFalse(authService.logout("old_token"));
+    }
+
+	@Test
+	void logout_WithouthLoginDone_ShouldReturnFalse() {
+		String token = "token";
+		assertFalse(authService.logout(token));
+		verify(clientRepository, times(0)).findByEmail(any(String.class));
+	}
+
+	@Test
+	void getCurrentUser_WithValidToken_ShouldReturnCurrentUser() {
+		String token = "valid_token";
+		String email = "test@example.com";
+		Client client = new Client();
+		client.setEmail(email);
+		ClientDTO expectedClientDTO = new ClientDTO();
+		expectedClientDTO.setEmail(email);
+		when(jwtTokenService.getEmailFromToken(token)).thenReturn(email);
+		when(clientRepository.findByEmail(email)).thenReturn(Optional.of(client));
+		ClientDTO result = authService.currentUser(token);
+		assertNotNull(result);
+		assertEquals(expectedClientDTO, result);
+		verify(jwtTokenService, times(1)).getEmailFromToken(token);
+		verify(clientRepository, times(1)).findByEmail(email);
+	}
+
+	@Test
+	 void testCurrentUser_InvalidToken_ReturnsNull() {
+		String token = "invalid_token";
+		when(jwtTokenService.getEmailFromToken(token)).thenReturn(null);
+		ClientDTO result = authService.currentUser(token);
+		assertNull(result);
+		verify(jwtTokenService, times(1)).getEmailFromToken(token);
+		verify(clientRepository, never()).findByEmail(anyString());
+	}
+
+}
